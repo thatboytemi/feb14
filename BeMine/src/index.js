cdf8bc5105b2ba59fab7f0ed34384ee0d62a4cef
thatboytemi <144232256+thatboytemi@users.noreply.github.com>
2024-01-31 20:11:50 +0200

    Delete BeMine directory

--- BeMine/src/index.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("no");
-button.addEventListener("mouseover", (event)=>{
-    const randomX = Math.random()*window.innerWidth +"px";
-    const randomY = Math.random()*window.innerHeight +"px";
-    event.target.setAttribute("style", `position:absolute; top:${randomY}; right:${randomX}`);
-});
